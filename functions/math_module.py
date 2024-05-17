64a02af4ff6473b437b79a2db1007c9ebee3eaa5
박희진(Heejin Park) <A202102014@RGPKorea.com>
2024-05-17 15:57:08 +0900

    first commit

---
+++ functions/math_module.py
@@ -0,0 +1,11 @@
+
+class test:
+    pass
+
+
+class test12:
+    pass
+
+
+def add(a, b):
+    return a + b
---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+def main():
+    #  프로그램의 주요 실행 코드를 작성
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    # main.py가 직접 실행될 때만 실행 됨
+    main()
+
+
